1c076b11706af860058f8e89e169558878c3aa3e
obijoyce@gmail.com <obijoyce@gmail.com>
2018-08-18 05:44:17 +0100

    add export app to app.js

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ import router from './server/routes/questionsRoute';
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+console.log('HEREER');
 app.use('/api/v1', router);
+
 
 const port = process.env.PORT || 3300;
 
 app.listen(port, () => console.log(`App is listening on port ${port}`));
+
+
+export default app;
