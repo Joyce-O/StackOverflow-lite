1c46926d8bb82a1b2b84b1af52347451a4f5fed2
obijoyce@gmail.com <obijoyce@gmail.com>
2018-08-18 13:21:21 +0100

    Add post answer handler to controller

--- server/controllers/QuestionsHandler.js
+++ server/controllers/QuestionsHandler.js
@@ -33,7 +33,33 @@ class QuestionsHandler {
             message: 'Question has been recorded',
             newQuestion
           });
-      }
+    }
+
+    static getQuestion(request, response) {
+      const { requestedQues } = request.body;
+      response.status(200)
+        .json({
+          message: 'Request was successful',
+          requestedQues
+        });
+    }
+  
+    static postAnswer(request, response) {
+      const { newAnswer, requestedQues } = request.body;
+      const id = requestedQues.answer.length + 1;
+      const submittedAnswer = {
+        id,
+        newAnswer
+      };
+      requestedQues.answer.push(submittedAnswer);
+      requestedQues.answer.reverse();
+      return response.status(201)
+        .json({
+          message: 'Thank you! Your answer was recorded',
+          requestedQues
+        });
+    }
+  
     
 }
 
