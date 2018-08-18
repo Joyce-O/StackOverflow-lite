3180473b2fff855988d68d8d50faff5ad0f49fcc
obijoyce@gmail.com <obijoyce@gmail.com>
2018-08-18 15:13:16 +0100

    Add delete route to questionRoute

--- server/routes/questionsRoute.js
+++ server/routes/questionsRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import questionsClass from '../controllers/QuestionsHandler';
 import QuestionsValidator from '../middlewares/QuestionsValidator';
 
-const { getAllQuestions, getQuestion, postQuestion, postAnswer } = questionsClass;
+const { getAllQuestions, getQuestion, postQuestion, postAnswer, deleteQuestion} = questionsClass;
 const { getQuestionValidator, postQuestionValidator, postAnswerValidator } = QuestionsValidator;
 
 
@@ -12,6 +12,7 @@ router.get('/questions', getAllQuestions);
 router.get('/questions/:questionId', getQuestionValidator,getQuestion);
 router.post('/questions', postQuestionValidator, postQuestion);
 router.post('/questions/:questionId/answers', getQuestionValidator, postAnswerValidator, postAnswer);
+router.delete('/questions/:questionId', getQuestionValidator, deleteQuestion);
 
 router.get('/', (request, response) => {
     response.status(200)
