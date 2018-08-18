a526ea363a58b38ecfda22efcd825ae54a893141
obijoyce@gmail.com <obijoyce@gmail.com>
2018-08-18 13:25:52 +0100

    Add answer validator to middleware.

--- server/middlewares/QuestionsValidator.js
+++ server/middlewares/QuestionsValidator.js
@@ -93,6 +93,34 @@ class QuestionsValidator {
         request.body.tag = tag;
         next();
       } // End post question Validation
+
+      static postAnswerValidator(request, response, next) {
+        let { newAnswer } = request.body;
+        if (newAnswer === undefined) {
+          return response.status(400)
+            .json({
+              status: 'Error',
+              message: 'Answer cannot be undefined'
+            });
+        }
+        newAnswer = newAnswer.trim();
+        if (newAnswer === '') {
+          return response.status(400)
+            .json({
+              status: 'Error',
+              message: 'Answer cannot be empty'
+            });
+        }
+        if (newAnswer.length < 3 || newAnswer.length > 300) {
+          return response.status(400)
+            .json({
+              status: 'Error',
+              message: 'Answer should be 3 to 300 characters long'
+            });
+        }
+        request.body.newAnswer = newAnswer;
+        next();//End Answer validator 
+      }
     
 }
 
