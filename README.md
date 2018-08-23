084ac370c2f67cb6d12506f7f98df62c73527ca4
obijoyce@gmail.com <obijoyce@gmail.com>
2018-08-23 19:28:26 +0100

    feat(refracor UI): add dropdown menu to profile [Finishes gh-pages]

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![Build Status](https://travis-ci.org/Joyce-O/StackOverflow-lite.svg?branch=dev)](https://travis-ci.org/Joyce-O/StackOverflow-lite) [![Coverage Status](https://coveralls.io/repos/github/Joyce-O/StackOverflow-lite/badge.svg?branch=dev)](https://coveralls.io/github/Joyce-O/StackOverflow-lite?branch=dev)  [![Maintainability](https://api.codeclimate.com/v1/badges/244e7769d7f70afe94a3/maintainability)](https://codeclimate.com/github/Joyce-O/StackOverflow-lite/maintainability)
+
 # StackOverflow-lite
 StackOverflow-lite is a platform where people can ask questions and provide answers.
+
---
+++ server/routes/questionsRoute.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import questionsClass from '../controllers/QuestionsHandler';
+import QuestionsValidator from '../middlewares/QuestionsValidator';
+
+const { getAllQuestions, getQuestion, postQuestion, postAnswer, deleteQuestion } = questionsClass;
+const { getQuestionValidator, postQuestionValidator, postAnswerValidator,  } = QuestionsValidator;
+
+const router = express.Router();
+
+router.get('/questions', getAllQuestions);
+router.get('/questions/:questionId', getQuestionValidator, getQuestion);
+router.post('/questions', postQuestionValidator, postQuestion);
+router.post('/questions/:questionId/answers', getQuestionValidator, postAnswerValidator, postAnswer);
+router.delete('/questions/:questionId', getQuestionValidator, deleteQuestion);
+
+router.get('/', (request, response) => {
+    response.status(200)
+      .json({
+        message: 'This is stackOverflow-lite. Post questions, view questions, post answers, and view answers to question'
+      });
+  });
+
+export default router;
