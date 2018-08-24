14fc450fda517b8f559eaa7879b69cf8a5853829
obijoyce@gmail.com <obijoyce@gmail.com>
2018-08-24 09:33:29 +0100

    feat(user login): create user login handler that persist in database [finishes #160014236]

--- server/controllers/usersHandler.js
+++ server/controllers/usersHandler.js
@@ -22,5 +22,40 @@ class UserHandler {
         console.log('CATCH ERROR:', error.message);
       });
   }
+
+  static loginUser(request, response) {
+    const { username, password } = request.body;
+
+    const sqlQuery = 'select * from users where username = $1';
+    const variable = [username];
+
+    pool.query(sqlQuery, variable)
+      .then((result) => {
+        console.log('LOGIN RESULT', result);
+
+        const userData = result.rows[0];
+        if (result.rowCount === 1) {
+          if (password !== userData.password) {
+            return response.status(403)
+              .json({
+                status: 'Error',
+                message: 'Incorrect password'
+              });
+          }
+          return response.status(200)
+            .json({
+              message: `Welcome back ${userData.username}`
+            });
+        }
+        return response.status(401)
+          .json({
+            message: 'User does not exist in database',
+            status: 'Error'
+          });
+      })
+      .catch((error) => {
+        console.log('CATCH ERROR:', error.message);
+      });
+  }
 }
 export default UserHandler;
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,10 +2,11 @@ import express from 'express';
 import userClass from '../controllers/usersHandler';
 
 
-const { signupUser } = userClass;
+const { signupUser, loginUser } = userClass;
 
 const router = express.Router();
 
 router.post('/auth/signup', signupUser);
+router.post('/auth/login', loginUser);
 
 export default router;
